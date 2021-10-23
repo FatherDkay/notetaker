79ad00bf2c6c2b9fd6487221fa969ce7acca7a15
FatherDkay <paul.d.bailey@hotmail.com>
2021-10-23 15:59:17 -0700

    server html success

--- server.js
+++ server.js
@@ -86,6 +86,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
   });
 
+  app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+  });
+
 //End routes
 
 app.listen(PORT, () => {
